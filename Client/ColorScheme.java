13f670989ce10e9c9bd84b729b46651e6cb23bf5
EddieDevine <edevine2025@cchsdons.com>
2025-05-27 09:03:08 -0700

    Delete Client/ColorScheme.java

--- Client/ColorScheme.java
+++
@@ -1,9 +0,0 @@
-package Client;
-
-import java.awt.*;
-
-public class ColorScheme {
-    //Cathedral Catholic Colors
-    public static final Color CATHEDRAL_RED = Color.decode("#C20F2F");
-    public static final Color CATHEDRAL_GOLD = Color.decode("#F9BF3C");
-}
